ea1bce24aba8f02f1c12b68a566a5416bc29912d
jpaddeo <jpaddeo@gmail.com>
2023-01-18 10:23:29 -0300

    feat(db): agrego types según review de @phreakphreak

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,10 +4,12 @@ import { PrismaClient } from '@prisma/client';
 
 const data = JSON.parse(fs.readFileSync('./prisma/data.json', 'utf-8'));
 
+import { type Data } from './data.dto';
+
 const prisma = new PrismaClient();
 
 async function main() {
-  data.forEach(async (d: any) => {
+  data.forEach(async (d: Data) => {
     await prisma.invoice.create({
       data: {
         paymentDue: new Date(d.paymentDue),
