5407f344f7c73ba4f5e22f5eb98f35be7cf09b67
jpaddeo <jpaddeo@gmail.com>
2023-01-20 11:42:42 -0300

    refactor(db): :recycle: eslinteo código con configuración de branch main

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,17 @@
-import fs from 'fs';
+import fs from 'fs'
 
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const data = JSON.parse(fs.readFileSync('./prisma/data.json', 'utf-8'));
+const data = JSON.parse(fs.readFileSync('./prisma/data.json', 'utf-8'))
 
-import { type Data } from './data.dto';
+import { type Data } from './data.dto'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 async function main() {
   await prisma.$transaction(
-    data.map((d: Data) =>  prisma.invoice.upsert({
+    data.map((d: Data) =>
+      prisma.invoice.upsert({
         where: { id: d.id },
         update: {},
         create: {
@@ -24,8 +25,8 @@ async function main() {
               street: d.senderAddress.street,
               city: d.senderAddress.city,
               postCode: d.senderAddress.postCode,
-              country: d.senderAddress.country,
-            },
+              country: d.senderAddress.country
+            }
           },
           client: {
             create: {
@@ -36,24 +37,24 @@ async function main() {
                   street: d.clientAddress.street,
                   city: d.clientAddress.city,
                   postCode: d.clientAddress.postCode,
-                  country: d.clientAddress.country,
-                },
-              },
-            },
+                  country: d.clientAddress.country
+                }
+              }
+            }
           },
           items: {
-            create: [...d.items],
-          },
-        },
+            create: [...d.items]
+          }
+        }
       })
     )
-  );
+  )
 }
 
 main()
   .then(async () => await prisma.$disconnect())
   .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
