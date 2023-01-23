796eaa2c5756e77d83c4befdc33f92bc21360808
Jean-Dv <mrjunior127@gmail.com>
2023-01-23 17:32:08 -0500

    chore(eslint): :rotating_light: elimine la configuración global de linter (no-misused-promises)
    Además agregue que eslint ignore el archivo entero de router.ts, queda pendiente el TODO

--- src/apps/invoices/router.ts
+++ src/apps/invoices/router.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* TODO: tocaría revisar si hay alguna forma mejor de resolver este problema, por el momento lo dejaré así */
 import { Router } from 'express'
 import { getAllInvoices } from './http'
 
