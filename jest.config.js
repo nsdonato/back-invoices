e5f01c84cfffa2c626ec5209fa15c9c7317589f4
Jean-Dv <mrjunior127@gmail.com>
2023-01-22 19:59:10 -0500

    refactor(aliases): :bug: agregue module aliases para un commit anterior

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,8 @@ module.exports = {
     moduleNameMapper: {
       '^@apps(.*)$': '<rootDir>/src/apps$1',
       '^@configs(.*)$': '<rootDir>/src/configs$1',
+      '^@middlewares(.*)$': '<rootDir>/src/middlewares$1',
+      '^@models(.*)$': '<rootDir>/src/models$1',
       '^@bin(.*)$': '<rootDir>/bin$1',
       '^@src(.*)$': '<rootDir>/src$1',
       '^@root(.*)$': '<rootDir>/$1'
